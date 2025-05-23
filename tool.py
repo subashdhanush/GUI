13c57b177c4787711b1944472ea7f2ef26337617
subashdhanush <85820233+subashdhanush@users.noreply.github.com>
2025-05-23 19:08:21 +0530

    Add files via upload

---
+++ tool.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Hello Tkinter")
+tk.Label(root, text="Welcome to Tkinter!").pack()
+tk.Label(root, text="Welcome to Tkinter!").pack()
+root.mainloop()
